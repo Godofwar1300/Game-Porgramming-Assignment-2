6da179ea9e602ae93e61b9bde46a60ab3dcc67b2
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2020-01-30 14:09:04 -0600

    Did some work on basic functionality

---
+++ Game Programming Assignment 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿/*****************************************************************************
+// File Name : GameManager.cs
+// Author : ADD AUTHOR HERE
+// Creation Date : ADD DATE HERE
+//
+// Brief Description : ADD DESCRIPTION HERE
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
