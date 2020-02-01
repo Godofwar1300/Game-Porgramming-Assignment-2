a6a23b1db05c5e2e65488923d417e7d555cd48d4
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2020-02-01 00:16:00 -0600

    Fixed some small things

---
+++ Class 2 Class Example/Assets/Scripts/ScreenshotHandler.cs
@@ -0,0 +1,46 @@
+﻿/*****************************************************************************
+// File Name : ScreenshotHandler.cs
+// Author : ADD AUTHOR HERE
+// Creation Date : ADD DATE HERE
+//
+// Brief Description : ADD DESCRIPTION HERE
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHandler : MonoBehaviour
+{
+
+    private static ScreenshotHandler instance;
+
+    private Camera myCamera;
+
+    private bool takeScreenshotOnNextframe;
+
+    private void Awake()
+    {
+        instance = this;
+        myCamera = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+     if(takeScreenshotOnNextframe)
+        {
+            takeScreenshotOnNextframe = false;
+            RenderTexture renderTexture = myCamera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+        }
+    }
+    private void TakeScreenshot(int width, int height)
+    {
+        myCamera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        takeScreenshotOnNextframe = true;
+    }
+
+
+}
